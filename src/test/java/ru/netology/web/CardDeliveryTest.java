e729b8c229e622473eade05a18c668917c52b3b6
Roman Kochnev <romaslog@icloud.com>
2023-10-03 13:44:29 +0500

    Tests failed

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -3,9 +3,13 @@ package ru.netology.web;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardDeliveryTest {
 
@@ -13,13 +17,13 @@ public class CardDeliveryTest {
     void shouldTest() {
         open("http://localhost:9999");
         $$("[data-test-id=notification]");
-        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $("[data-test-id=city] input").setValue("");
         $("[data-test-id=date] input").setValue("11.10.2023");
         $("[data-test-id=name] input").setValue("Геннадий Ветров");
         $("[data-test-id=phone] input").setValue("+79090091919");
         $("[data-test-id=agreement]").click();
-        $("button").click();
-        // $(withText("Встреча успешно забронирована")).shouldBe(visible);
+        $("button.button").click();
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
 
 
     }
